0c7e4ef1cd5f5348674fdeb48c70b7e92c83442c
HENIT0885 <henitchobisa0885@gmail.com>
2022-05-23 18:59:05 +0530

    Editor Config Engage

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,32 @@ import "./App.css";
 import logo from "./logo.png"
 import JSONInput from "react-json-editor-ajrm"
 import locale from 'react-json-editor-ajrm/locale/en'
+import testData from './testData';
+import Editor from "react-simple-code-editor";
+import { highlight, languages } from "prismjs/components/prism-core";
+import "prismjs/components/prism-clike";
+import "prismjs/components/prism-javascript";
+import "prismjs/themes/prism.css";
 
 import {
     StompSessionProvider,
     useSubscription,
 } from "react-stomp-hooks";
+
+
+const hightlightWithLineNumbers = (input, language) =>
+  highlight(input, language)
+    .split("\n")
+    .map((line, i) => `<span class='editorLineNumber'>${i + 1} </span>${line}`)
+    .join("\n");
+
 class App extends Component {
+
 
     state = {
         connectionURL: "",
         connectionStatus: "DISCONNECTED",
+        data: "\t // Input your JSON here"
     }
 
     updateInputValue = (evt) => {
@@ -26,8 +42,16 @@ class App extends Component {
         this.setState({ connectionStatus: "CONNECTED" });
     }
 
-    data = {
-        "hello": "My name is Henit",
+
+    handleDataChange = (evt) => {
+        console.log(evt);
+        this.setState
+            (
+                {
+                    data: JSON.parse(evt.json)
+                }
+            );
+        console.log(this.state.data);
     }
 
 
@@ -52,35 +76,54 @@ class App extends Component {
                     <div className='subscriptionBar'>
                         <div className='subsTitleBar'>
                             Send Message
-                            {/* <div>
-                                <input type="text" className='subAddText' />
-                                <button>
-                                    Add
-                                </button>
-                            </div> */}
                         </div>
                         <div className='subsEditor'>
-                            <JSONInput
-                                id='a_unique_id'
-                                placeholder={this.data}
-                                locale={locale}
-                                height="100%"
-                                width="100%"
-                                // margin="0.5%"
+                            <Editor
+                            className='seditor'
+                                value={this.state.data}
+                                textareaId = "codeArea"
+                                onValueChange={(code) => this.setState({data : code})}
+                                highlight={(code) => hightlightWithLineNumbers(code, languages.js)}
+                                padding={10}
                             />
                         </div>
-                        
+
                         <div className='senderBar'>
                             <input className='channelInputBar' type="text" name="" id="" placeholder='Enter the channel to send' />
                             <button className='channelSendButton'>
                                 Send
                             </button>
-
                         </div>
 
                     </div>
                     <div className='resultBar'>
+                        <div className='resTitleBar'>
+                            Hearings
+                            <div className='subsInp'>
+                                <input type="text" placeholder='Add Subscriptions here' />
+                                <button>+</button>
+                            </div>
+                        </div>
 
+                        <div className='subsList'>
+                            <div className='subsItem'>
+                                My name is Henit
+                                <button >
+                                    x
+                                </button>
+                            </div>
+                        </div>
+
+                        <div className='resultEditor'>
+                            <JSONInput
+                                id='a_unique_id'
+                                placeholder={testData}
+                                locale={locale}
+                                height="100%"
+                                width="100%"
+                            // margin="0.5%"
+                            />
+                        </div>
                     </div>
                 </div>
                 <div className='bottomTitle'>
