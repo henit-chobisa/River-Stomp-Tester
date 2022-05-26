69fc75c98e5e3381d31bf8bcfdd4fc257fb89f48
HENIT0885 <henitchobisa0885@gmail.com>
2022-05-26 18:51:17 +0530

    test data updated

--- src/testData.js
+++ src/testData.js
@@ -1,6 +1,9 @@
 
 const testData = {
-    "Hello" : "This is River"
+    "App" : "River",
+    "Version" : "v1.0.0",
+    "License" : "MIT License",
+    "Developer" : "Henit Chobisa"
 }
 
 export default testData;
