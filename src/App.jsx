f8d89322e6399cfc9cf972352d2b37208cc8bc3a
HENIT0885 <henitchobisa0885@gmail.com>
2022-05-27 18:27:07 +0530

    Multiroute Function Added

--- src/App.jsx
+++ src/App.jsx
@@ -12,23 +12,22 @@ import "prismjs/themes/prism.css";
 import SubsItem from './SubsItem';
 import { useState } from 'react';
 import bell from "./bell.wav";
+import RouteItem from './routeItem';
 
-const hightlightWithLineNumbers = (input, language) =>
-    highlight(input, language)
-        .split("\n")
-        .map((line, i) => `<span class='editorLineNumber'>${i + 1}\t</span>${line}`)
-        .join("\n");
+const hightlightWithLineNumbers = (input, language) =>{
+    return highlight(input, language).split("\n").map((line, i) => `<span class='editorLineNumber'>${i + 1}\t</span>${line}`).join("\n")};
 
 
 
 const App = (props) => {
     var audio = new Audio(bell);
     const [connectionURL, updateConnectionURL] = useState(props.conURL);
-    const [data, updateData] = useState(" // Insert your message here");
-    const [header, updateHeader] = useState(" // Insert your headers here");
-    const [sendRoutes, updateSendRoutes] = useState([]);
+    const [data, updateData] = useState("{\n\t// Insert your message's body here\n}");
+    const [header, updateHeader] = useState("{\n\t// Insert your message's headers here\n}");
+    const [routeValue, updateRouteValue] = useState("");
+    const [routes, updateRoutes] = useState([]);
     const [resultData, updateResultData] = useState(testData);
-    const [sendRoute, updateSendRoute] = useState("");
+    // const [sendRoute, updateSendRoute] = useState("");
     const [subscriptions, updateSubscriptions] = useState([]);
     const [subsText, updateSubsText] = useState("");
     const [currentRoute, updateCurrentRoute] = useState("Current Route (Scrollable)")
@@ -36,14 +35,12 @@ const App = (props) => {
     const isConnected = props.isConnected;
     const client = props.getClient();
 
-    const updateSendRouteValue = (evt) => {
-        updateSendRoute(evt.target.value)
-    }
+
     const handleSendEvent = () => {
-        client?.publish({
-            destination: sendRoute,
-            body: JSON.stringify(JSON.parse(data))
-        })
+        // client?.publish({
+        //     destination: sendRoute,
+        //     body: JSON.stringify(JSON.parse(data))
+        // })
     }
     const updateInputValue = (evt) => {
         updateConnectionURL(evt.target.value);
@@ -110,7 +107,6 @@ const App = (props) => {
             var clone = [...subscriptions];
             clone.map((sub) =>
                 sub.isActive = false
-
             )
             clone[index].isActive = true;
             updateCurrentCounter(clone[index].counter);
@@ -145,6 +141,64 @@ const App = (props) => {
         audio.play();
     }
 
+    const handleRouteValueChange = (evt) => {
+        updateRouteValue(evt.target.value);
+    }
+
+    const addRoute = () => {
+        var clone = [...routes];
+        clone.push({value : routeValue, index : clone.length, body: "{\n\t// Insert your message's body here\n}", header: "{\n\t// Insert your message's headers here\n}", isActive: false});
+        updateRoutes(clone);
+        updateRouteValue("");
+    }
+
+    const renderRoutes = () => {
+        if (routes.length === 0){
+            return (<p className='warning'> No Routes added yet, add one. </p>)
+        }
+        else {
+            return routes.map((route) => (<RouteItem route={route.value} index={route.index}  isActive={route.isActive} handleRoutePop={handleRoutePop} handleSelection={handleRouteItemSelection}/>));
+        }
+    }
+
+    const handleRoutePop = (index) => {
+        var clone = [...routes];
+        var wasActive = clone[index].isActive;
+        clone.splice(index, 1);
+        if (wasActive === true) {
+            if (clone.length > 0) {
+                if (index === 0) {
+                    clone[index].isActive = true;
+                    updateData(clone[index].body);
+                    updateHeader(clone[index].header);
+                }
+                else {
+                    clone[index - 1].isActive = true;
+                    updateData(clone[index - 1].body);
+                    updateHeader(clone[index - 1].header);
+                }
+            }
+            else {
+                updateData("{\n\t// Insert your message's body here\n}")
+                updateHeader("{\n\t// Insert your message's headers here\n}")
+            }
+        }
+        updateRoutes(clone);
+    }
+
+    const handleRouteItemSelection = (index, target) => {
+        if (target.tagName === "DIV"){
+            var clone = [...routes];
+            clone.map((route) =>
+                route.isActive = false
+            )
+            clone[index].isActive = true;
+            updateData(clone[index].body);
+            updateHeader(clone[index].header);
+            updateRoutes(clone);
+        }
+    }
+
 
     return (
         <div className='App'>
@@ -165,8 +219,8 @@ const App = (props) => {
                     <div className='subsTitleBar'>
                         Available Routes
                         <div className="routeManager">
-                            <input type="text" name="" id="" placeholder='Add a route here' />
-                            <button>
+                            <input type="text" name="" id="" value={routeValue} onChange={handleRouteValueChange} placeholder='Add a route here' />
+                            <button onClick={addRoute}>
                                 +
                             </button>
                         </div>
@@ -176,11 +230,7 @@ const App = (props) => {
                             Send
                         </button>
                         <div className="routeList">
-                            <div className="routeItem">
-                                <p>Hello</p>
-                                <button>x</button>
-                            </div>
-
+                            {renderRoutes()}
                         </div>
                     </div>
                     <div className='subsEditor'>
---
+++ src/routeItem.jsx
@@ -0,0 +1,34 @@
+const routeItem = (props) => {
+
+    const handleSelfClick = (evt) => {
+        props.handleSelection(props.index, evt.target);
+    }
+
+    const activation = () => {
+        if(props.isActive === true){
+            return {
+                backgroundColor: "rgb(239, 118, 122)"
+            }
+        }
+        else {
+            return {
+                backgroundColor: "rgb(0, 160, 198)"
+            }
+
+        }
+    }
+
+    const handleSelfDelete = () => {
+        props.handleRoutePop(props.index);
+    }
+
+    return (
+        <div className="routeItem" style={activation()} onClick={handleSelfClick}>
+            <p>{props.route}</p>
+            <button onClick={handleSelfDelete}>x</button>
+        </div>
+
+    )
+}
+
+export default routeItem;
