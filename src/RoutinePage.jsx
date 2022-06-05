a5e63de84b77c341b257a312b7a77d84ad5e3a1b
HENIT0885 <henitchobisa0885@gmail.com>
2022-06-05 19:15:52 +0530

    Routine Page engaged

--- src/RoutinePage.jsx
+++ src/RoutinePage.jsx
@@ -1,10 +1,42 @@
 import { useState } from "react"
+import "./Styles/RoutinePage.css"
 
 const RoutinePage = (props) => {
 
     return (
-        <div>
-            <p style={{color:"white"}}>Hello World</p>
+        <div className="routinePage">
+            <div className="titleBar">
+                <h2>Routines</h2>
+                <div className="createRoutine">
+                    <p>Create Routine</p>
+                </div>
+            </div>
+            <div className="routineList">
+                <div className="routineItem">
+
+                </div>
+                <div className="routineItem">
+
+                </div>
+                <div className="routineItem">
+
+                </div>
+                <div className="routineItem">
+
+                </div>
+                <div className="routineItem">
+
+                </div>
+                <div className="routineItem">
+
+                </div>
+                <div className="routineItem">
+
+                </div>
+                <div className="routineItem">
+
+                </div>
+            </div>
         </div>
     );
 }
