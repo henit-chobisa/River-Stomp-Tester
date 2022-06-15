1a202cbb95a77d090c6debf48cc6b1731a34bf8d
HENIT0885 <henitchobisa0885@gmail.com>
2022-06-15 18:55:52 +0530

    feat: Routine Display Container Setup

---
+++ src/RoutineExecPage.jsx
@@ -0,0 +1,23 @@
+import React, { useEffect } from "react";
+import ReactDOM from "react-dom";
+
+const RoutineExecPage = (props) => {
+
+    const container = document.createElement('div');
+    var externalWindow = null;
+
+    useEffect(() => {
+        externalWindow = window.open('', 'RoutineExecPage');
+        if (externalWindow){
+            externalWindow.document.body.appendChild(container);
+            externalWindow.onunload = () => {
+            }
+        }
+    })
+
+    return (
+        ReactDOM.createPortal((props.children), container)
+    )
+}
+
+export default RoutineExecPage;
