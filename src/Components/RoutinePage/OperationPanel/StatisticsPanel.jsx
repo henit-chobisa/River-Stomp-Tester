a01859249bb2686aae5f868cdd573b050880c94d
HENIT0885 <henitchobisa0885@gmail.com>
2022-06-16 18:06:04 +0530

    feat:  Stats Panel Detached

---
+++ src/Components/RoutinePage/OperationPanel/StatisticsPanel.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import '../../../Styles/RoutinePage/OperationPanel/StatisticsPanel.css'
+
+const StatisticsPanel = (props) => {
+    return (
+        <div className="statsPanelDiv">
+            <div className="featureBar">
+
+            </div>
+            <div className="presentationBar">
+
+            </div>
+        </div>
+    )
+}
+
+export default StatisticsPanel;
