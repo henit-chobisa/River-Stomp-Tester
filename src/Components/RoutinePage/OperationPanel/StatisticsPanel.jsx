ebd1f725c739c07d91f13c652947039248390ddb
HENIT0885 <henitchobisa0885@gmail.com>
2022-06-16 18:59:32 +0530

    feat: Statistics Panel Modified for Bottom Title

--- src/Components/RoutinePage/OperationPanel/StatisticsPanel.jsx
+++ src/Components/RoutinePage/OperationPanel/StatisticsPanel.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import '../../../Styles/RoutinePage/OperationPanel/StatisticsPanel.css'
+import OptionButton from "./OptionButton";
 
 const StatisticsPanel = (props) => {
     return (
         <div className="statsPanelDiv">
             <div className="featureBar">
-
+                <OptionButton title={"Presentation"}/>
             </div>
             <div className="presentationBar">
 
