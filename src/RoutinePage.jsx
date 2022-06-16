edad3b630b9a9bcbeddc0f6e7b62f8721bc26cb7
HENIT0885 <henitchobisa0885@gmail.com>
2022-06-16 15:56:57 +0530

    feat: Added Operations Container

--- src/RoutinePage.jsx
+++ src/RoutinePage.jsx
@@ -3,16 +3,46 @@ import RoutineItem from "./Components/RoutinePage/RoutineItem";
 import "./Styles/RoutinePage.css"
 import SideBar from "./Components/RoutinePage/SideBar";
 import React from 'react';
+import PlusButton from "./Components/RoutinePage/PlusButton";
 
 const RoutinePage = (props) => {
+
+    const [routines, updateRoutines] = useState([]);
+
+    const renderRoutineItems = () => {
+        if (routines.length === 0 ) {
+            return (
+                <RoutineItem present={false}/>
+            )
+        }
+        else {
+            return routines.map((routine) => {
+                return <RoutineItem present={true} title={routine.title} lastUpdated={routine.lastUpdated} />
+            })
+        }
+    }
 
     return (
         <div className="routinePage">
             <div className="routinesBar">
                 <SideBar id={"routineSideBar"} title={"Routines"}/>
                 <div className="routinesGroup">
-                    <RoutineItem present={true}/>
+                    {renderRoutineItems()}
                 </div>
+                <div className="utilDivider"></div>
+                <div className="utilsDiv">
+                    <PlusButton></PlusButton>
+                </div>
+
+            </div>
+            <div className="operationsBar">
+                <div className="statisticsPanel">
+
+                </div>
+                <div className="observationsPanel">
+                    
+                </div>
+
             </div>
 
             {/* <div className="routineList">
