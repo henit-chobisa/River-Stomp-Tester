dcbe365879aed495bf547811d83cb5319690f7dd
HENIT0885 <henitchobisa0885@gmail.com>
2022-06-16 18:05:29 +0530

    feat: OperationPanel Modified for elements

--- src/Components/RoutinePage/OperationPanel.jsx
+++ src/Components/RoutinePage/OperationPanel.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import '../../Styles/RoutinePage/OperationsPanel.css'
+import StatisticsPanel from "./OperationPanel/StatisticsPanel";
+import ObservationPanel from "./OperationPanel/ObservationPanel";
 
 const OperationsPanel = () => {
 
     return (
         <div className="panel">
             <div className="statisticsPanel">
-
+                <StatisticsPanel/>
             </div>
             <div className="observationsPanel">
-
+                <ObservationPanel/>
             </div>
         </div>
     )
