aaee00c94e6ed843d5e9ecf230d85cc003d951d0
HENIT0885 <henitchobisa0885@gmail.com>
2022-06-19 18:24:27 +0530

    feat: Slide Right on Add Implied On Routines Group

--- src/RoutinePage.jsx
+++ src/RoutinePage.jsx
@@ -5,11 +5,16 @@ import SideBar from "./Components/RoutinePage/SideBar";
 import React from 'react';
 import PlusButton from "./Components/RoutinePage/PlusButton";
 import OperationsPanel from "./Components/RoutinePage/OperationPanel";
+import { useRef } from "react";
+import { useEffect } from "react";
 
 const RoutinePage = (props) => {
 
     const [routines, updateRoutines] = useState([]);
     const [selectedRoutine, updateSelectedRoutine] = useState(null);
+    const [routineChange, updateRoutineChange] = useState(0);
+    // O for all good and 1 for something is changed;
+    const routineList = useRef();
 
     const renderRoutineItems = () => {
         if (routines.length === 0) {
@@ -23,6 +28,13 @@ const RoutinePage = (props) => {
             });
         }
     }
+
+    useEffect(() => {
+        if (routineChange === 1){
+            routineList.current.getElementsByClassName("routineItem")[routines.length - 1].scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
+            updateRoutineChange(0);
+        }
+    }, [routineChange, routines])
 
     const handleRoutineItemDelete = (index) => {
         const clone = [...routines];
@@ -40,6 +52,7 @@ const RoutinePage = (props) => {
         }
 
         clone[index].isActive = true;
+        
         updateSelectedRoutine(index);
         updateRoutines(clone);
     }
@@ -56,6 +69,7 @@ const RoutinePage = (props) => {
         if (contains === false) {
             clone.push(routine);
             updateRoutines(clone);
+            updateRoutineChange(1);
         }
     }
 
@@ -63,7 +77,7 @@ const RoutinePage = (props) => {
         <div className="routinePage">
             <div className="routinesBar">
                 <SideBar id={"routineSideBar"} title={"Routines"} />
-                <div className="routinesGroup">
+                <div className="routinesGroup" ref={routineList}>
                     {renderRoutineItems()}
                 </div>
                 <div className="utilDivider"></div>
