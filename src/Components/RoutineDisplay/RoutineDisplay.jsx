705c0bc3e260ec18ec9362353a33ba9397c5dce5
HENIT0885 <henitchobisa0885@gmail.com>
2022-06-22 20:01:13 +0530

    feat: Routine Display Added with Option Panel & Message Panel

--- src/Components/RoutineDisplay/RoutineDisplay.jsx
+++ src/Components/RoutineDisplay/RoutineDisplay.jsx
@@ -5,9 +5,52 @@ import logo from '../../Assets/logo.png'
 import { CircularProgress } from "@mui/material";
 import theme from "../Generic/Theme";
 import { ThemeProvider } from "@mui/system";
+import OptionButton from "../RoutinePage/OperationPanel/OptionButton";
+import testRoutineData from "../../Assets/testRoutineData";
+import OptionWrapper from "./Components/OptionWrapper";
 
 const RoutineDisplay = () => {
     const [searchParams, updateSearchParams] = useSearchParams();
+    const [selectedIndex, updateSelectedIndex] = useState(null);
+    const [options, updateOptions] = useState([
+        {title: "Routine Map", isSelected: false},
+        {title: "Statistics", isSelected: false},
+        {title: "Observations", isSelected: false},
+        {title: "Create Subroutine", isSelected: false}
+    ]);
+
+    const handleOptionClickCallback = (index) => {
+        const clone = [...options];
+        if (selectedIndex !== null) {
+            clone[selectedIndex].isSelected = false;
+        }
+        updateSelectedIndex(index);
+        clone[index].isSelected = true;
+        updateOptions(clone);
+    }
+
+    const renderOptions = () => {
+        return options.map((option, index) => (<OptionButton key={index} onClickOption={handleOptionClickCallback} index={index} isSelected={option.isSelected} title={option.title} />));
+    }
+  
+    const renderOptionComponent = () => {
+        if (selectedIndex === null){
+            return (
+            <div className="nullOptionWarning">
+                <p>Select an option to display :)</p>
+            </div>
+            )
+        }
+        else {
+            return (
+                <div className="OptionView">
+                    <OptionWrapper data={testRoutineData} selection={selectedIndex}/>
+                </div>
+            )
+        }
+        
+    }
+
 
     const getSearchParams = () => {
         console.log(searchParams.get("routineID"));
@@ -31,7 +74,13 @@ const RoutineDisplay = () => {
             </div>
             <div className="middleBar">
                 <div className="utilities">
-
+                    <div className="display">
+                        {renderOptionComponent()}
+                    </div>
+                    <div className="divider"></div>
+                    <div className="options">
+                        {renderOptions()}
+                    </div>
                 </div>
                 <div className="subRoutines">
 
