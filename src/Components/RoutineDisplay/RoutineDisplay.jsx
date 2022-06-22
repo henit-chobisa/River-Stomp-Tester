981b8297956c1810d787d718e081d2ffa3f8979e
HENIT0885 <henitchobisa0885@gmail.com>
2022-06-22 16:40:10 +0530

    feat: End Bar UI Completed with message bar

--- src/Components/RoutineDisplay/RoutineDisplay.jsx
+++ src/Components/RoutineDisplay/RoutineDisplay.jsx
@@ -2,6 +2,9 @@ import React, { useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import '../../Styles/RoutineDisplay/RoutineDispMain.css'
 import logo from '../../Assets/logo.png'
+import { CircularProgress } from "@mui/material";
+import theme from "../Generic/Theme";
+import { ThemeProvider } from "@mui/system";
 
 const RoutineDisplay = () => {
     const [searchParams, updateSearchParams] = useSearchParams();
@@ -14,8 +17,16 @@ const RoutineDisplay = () => {
     return (
         <div className="routineDisplay">
             <div className="topBar">
-                <div className="logo">
-                    <img className="logoIMG" src={logo} alt="" />
+                <div className="leftBar">
+                    <div className="logo">
+                        <img className="logoIMG" src={logo} alt="" />
+                    </div>
+                    <div className="routineName">
+                        <p>Routine Name</p>
+                    </div>
+                </div>
+                <div className="rightBarTitle">
+                    R i v e r
                 </div>
             </div>
             <div className="middleBar">
@@ -23,11 +34,24 @@ const RoutineDisplay = () => {
 
                 </div>
                 <div className="subRoutines">
-                    
+
                 </div>
             </div>
             <div className="endBar">
-
+                <div className="messages">
+                    <p className="messageTitle">Initiating Subscriptions...</p>
+                    <ThemeProvider theme={theme}>
+                        <CircularProgress color="primary" size={"15px"}/>
+                    </ThemeProvider>
+                </div>
+                <div className="connectionInfo">
+                    <div className="connectionStatus">
+                        <p>Connected</p>
+                    </div>
+                    <div className="connectionURL">
+                        <p>http://localhost:9000/disprout</p>
+                    </div>
+                </div>
             </div>
         </div>
 
