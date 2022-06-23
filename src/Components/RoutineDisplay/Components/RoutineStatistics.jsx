346467d5dd85d2b89b9749fffdd1e3eca7a5d9de
HENIT0885 <henitchobisa0885@gmail.com>
2022-06-23 16:35:07 +0530

    feat: Routine Statistics Implemented

--- src/Components/RoutineDisplay/Components/RoutineStatistics.jsx
+++ src/Components/RoutineDisplay/Components/RoutineStatistics.jsx
@@ -1,18 +1,70 @@
 import React from "react";
 import '../../../Styles/RoutineDisplay/Components/RoutineStatistics.css';
+import testRoutineData from "../../../Assets/testRoutineData";
+import { FlexibleXYPlot, VerticalBarSeries, XAxis, YAxis, LineSeries } from 'react-vis';
 
 const RoutineStatistics = () => {
+
+    const executionTimePublish = testRoutineData.routines.filter((data) => {
+        return data.operation === "PUBLISH"
+    }).map((data) => {
+        return { x: data.title, y: data.executionTime }
+    });
+
+    const executionTimeSubscribe = testRoutineData.routines.filter((data) => {
+        return data.operation !== "PUBLISH"
+    }).map((data) => {
+        return { x: data.title, y: data.executionTime }
+    });
+
+    const dataByteDataPublish = testRoutineData.routines.filter((dat) => {
+        return dat.operation === "PUBLISH"
+
+    }).map((dat) => {
+        return {
+            x: dat.title,
+            y: dat.dataBytes
+        }
+    });
+
+    const dataByteDataSubscribe = testRoutineData.routines.filter((dat) => {
+        return dat.operation !== "PUBLISH"
+    }).map((dat) => {
+        return {
+            x: dat.title,
+            y: dat.dataBytes
+        }
+    });
+
     return (
         <div className="routineStatistics">
-            
+
             <div className="dataBar">
-                
+                <p>Data Exchange(b) vs Routine</p>
+                <div className="graph">
+                    <FlexibleXYPlot className={"plot"} xType="ordinal">
+                        <XAxis />
+                        <YAxis />
+                        <VerticalBarSeries data={dataByteDataPublish} barWidth={0.4} opacity={1} color={"rgb(245, 61, 61)"} />
+                        <VerticalBarSeries data={dataByteDataSubscribe} barWidth={0.4} opacity={1} color={"rgb(0, 160, 198)"} />
+                    </FlexibleXYPlot>
+                </div>
 
             </div>
             <div className="execBar">
-
+                <p>Execution Time(ms) vs Routine</p>
+                <div className="graph2">
+                    <FlexibleXYPlot className={"plot"} xType="ordinal">
+                        <XAxis />
+                        <YAxis />
+                        <LineSeries data={executionTimePublish} color={"rgb(245, 61, 61)"}/>
+                        <LineSeries data={executionTimeSubscribe} color={"rgb(0, 160, 198)"} />
+                    </FlexibleXYPlot>
+                </div>
+                
             </div>
         </div>
+        // feat: Routine Statistics Implemented
         //feat: Skeleton Complete RoutineStatistics
     )
 }
