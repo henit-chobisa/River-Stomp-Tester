7b80183aa0838d3982ffa22d2ad568bd98891362
HENIT0885 <henitchobisa0885@gmail.com>
2022-06-23 15:45:00 +0530

    Skeleton Complete RoutineStatistics

--- src/Components/RoutineDisplay/Components/RoutineStatistics.jsx
+++ src/Components/RoutineDisplay/Components/RoutineStatistics.jsx
@@ -1,10 +1,19 @@
 import React from "react";
+import '../../../Styles/RoutineDisplay/Components/RoutineStatistics.css';
 
 const RoutineStatistics = () => {
     return (
         <div className="routineStatistics">
+            
+            <div className="dataBar">
+                
 
+            </div>
+            <div className="execBar">
+
+            </div>
         </div>
+        //feat: Skeleton Complete RoutineStatistics
     )
 }
 
