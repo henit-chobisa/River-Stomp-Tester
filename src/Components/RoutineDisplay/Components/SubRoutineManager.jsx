5ba3d1ef3d10f3952b500f253742f319eb2dc9bd
HENIT0885 <henitchobisa0885@gmail.com>
2022-06-28 18:02:58 +0530

    feat: Run Alone Configured in the SubRoutine Manger

--- src/Components/RoutineDisplay/Components/SubRoutineManager.jsx
+++ src/Components/RoutineDisplay/Components/SubRoutineManager.jsx
@@ -1,32 +1,58 @@
 import React, { useEffect } from "react";
 import { useState } from "react";
+import { useStompClient } from "react-stomp-hooks";
 import '../../../Styles/RoutineDisplay/Components/SubRoutineManager.css'
 import StatisticTitle from "../../Generic/StatisticTitle";
 import OptionButton from "../../RoutinePage/OperationPanel/OptionButton";
 import EditorComp from "../../SimpleTestingPage/EditorComp";
 var defaultHeader = "{\n\t\"message\" : \"Insert your header here\"\n}"
 var defaultBody = "{\n\t\"message\" : \"Insert your body here\"\n}";
-
+var defaultRunButtonStatus = "Run Alone";
 const SubRoutineManager = (props) => {
 
+    const [subRoutine, updateSubRoutine] = useState(props.SubRoutine);
     const [bodySelected, updateBodySelected] = useState(true);
     const handlePersistence = () => { }
     const [data, updateData] = useState(defaultBody);
     const [header, updateHeader] = useState(defaultHeader);
+    const [runButtonActive, updateRunButtonActive] = useState(true);
+    const [runButtonStatus, updateRunButtonStatus] = useState(defaultRunButtonStatus);
+    const stompClient = useStompClient();
 
     useEffect(() => {
-        if (props.SubRoutine !== undefined) {
-            if (props.SubRoutine.operation === "PUBLISH") {
-                updateData(props.SubRoutine.body);
-                updateHeader(props.SubRoutine.headers);
+        if (subRoutine !== undefined) {
+            if (subRoutine.operation === "PUBLISH") {
+                updateData(subRoutine.body);
+                updateHeader(subRoutine.headers);
             }
 
             else {
                 updateBodySelected(true);
-                updateData(props.SubRoutine.data);
+                updateData(subRoutine.data);
+                updateRunButtonActive(false);
             }
         }
-    })
+    });
+
+    // Run alone only valid for publish type subroutines
+
+    const runAlone = () => {
+        if (runButtonActive){
+            updateRunButtonActive(false);
+            updateRunButtonStatus("Running");
+            var startTime = performance.now();
+            stompClient.publish({destination: subRoutine.route, body: subRoutine.body, headers: subRoutine.headers});
+            var endTime = performance.now();
+            const executionTime = (Math.round((endTime - startTime) * 100) / 100).toFixed(2);
+            const dataExchange = (Buffer.from(subRoutine.body).length) + (Buffer.from(subRoutine.headers).length);
+            
+        
+            setTimeout(() => {
+                updateRunButtonActive(true);
+                updateRunButtonStatus(defaultRunButtonStatus);
+            }, 1500)
+        }
+    }
 
 
     const handleDataOptionClick = (index) => {
@@ -44,36 +70,36 @@ const SubRoutineManager = (props) => {
             <div className="basicEntities">
                 <div className="basicInfo">
                     <div className="idContainer">
-                        <p>{props.SubRoutine.id}</p>
+                        <p>{subRoutine.id}</p>
                     </div>
                     <div className="typeContainer">
-                        <p>{props.SubRoutine.operation}</p>
+                        <p>{subRoutine.operation}</p>
                     </div>
                 </div>
-                <div className="subRoutineFire">
-                    <p>Run Alone</p>
-                </div>
+                <div className="subRoutineFire" style={{opacity : runButtonActive === true ? 1 : 0.5}} onClick={runAlone}>
+                    <p>{runButtonStatus}</p>
+                </div> 
             </div>
 
             <div className="middleInfoBar">
                 <div className="leftBasicBar">
                     <div className="subRoutineTitleBar">
-                        <p>{props.SubRoutine.title}</p>
+                        <p>{subRoutine.title}</p>
                     </div>
                     <div className="subRoutineDescriptionBar">
-                        <p>{props.SubRoutine.description}</p>
+                        <p>{subRoutine.description}</p>
                     </div>
                     <div className="subRoutineStatsBar">
                         <div className="dataStats">
-                            <StatisticTitle heading={"Data Exchange"} num={props.SubRoutine.dataBytes} unit={"b"} />
+                            <StatisticTitle heading={"Data Exchange"} num={subRoutine.dataBytes} unit={"b"} />
                         </div>
                         <div className="speedStats">
-                            <StatisticTitle heading={"Execution Time"} num={props.SubRoutine.executionTime} unit={"ms"} />
+                            <StatisticTitle heading={"Execution Time"} num={subRoutine.executionTime} unit={"ms"} />
                         </div>
                     </div>
                     <div className="routeInfoBar">
                         <p>
-                            {props.SubRoutine.route}
+                            {subRoutine.route}
                         </p>
                     </div>
 
@@ -82,7 +108,7 @@ const SubRoutineManager = (props) => {
                 <div className="rightEditorBar">
                     <div className="optionsBar">
                         <OptionButton title={"Body"} isSelected={bodySelected} index={0} onClickOption={handleDataOptionClick} />
-                        {props.SubRoutine.operation === "PUBLISH" ? <OptionButton title={"Headers"} index={1} onClickOption={handleDataOptionClick} isSelected={!bodySelected} /> : <></>}
+                        {subRoutine.operation === "PUBLISH" ? <OptionButton title={"Headers"} index={1} onClickOption={handleDataOptionClick} isSelected={!bodySelected} /> : <></>}
                     </div>
                     <div className="mainEditor">
                         <EditorComp data={bodySelected ? data : header} handlePersistence={handlePersistence} updateData={bodySelected ? updateData : updateHeader} />
